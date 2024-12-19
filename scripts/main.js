12073b2e034e066ccb12a82b017c4d59ac24ed61
MuhammedMahameed212 <muhamma11@ac.sce.ac.il>
2024-12-19 18:15:59 +0200

    12

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
+let user = [
+    {
+    firstname: 'Muhammed',
+    lastname: 'Mahameed',
+    username: 'mhmdslah212',
+    password: '12345656',
+    age: '2024-2002'
+    },
+    {
+        firstname:"you"
+    },
+    {
+        firstname: false,
+        hehe:'hehe'
+    }
+
+];
+
+for(let i =0;i <= user.length;i++){
+    
+        console.log(`user ${i} is: ${user[i]);
+       
+}
+
+
+
 
